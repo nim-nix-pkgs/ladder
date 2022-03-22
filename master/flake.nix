459862c1bbb3eff3ba6c29ef7f379a20ebc43d6f
hugosenari <hugosenari@gmail.com>
2022-03-22 00:33:51 -0300

    chore: re-index ../nimpkgs/l/ladder

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Ladder logic macros for Nim'';
+  description = ''Ladder logic macros for Nim. documentation hosted at http://ryuk.ooo/nimdocs/ladder/ladder.html'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-ladder-master.flake = false;
   inputs.src-ladder-master.owner = "ryukoposting";
-  inputs.src-ladder-master.ref   = "refs/heads/master";
+  inputs.src-ladder-master.ref   = "master";
   inputs.src-ladder-master.repo  = "nim-ladder";
   inputs.src-ladder-master.type  = "gitlab";
   
